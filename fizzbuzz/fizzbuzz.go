9b697f8da84409026456d37eebdf8e90466028ef
kzame <ludovic.surel@gmail.com>
2023-01-09 12:45:17 +0400

    ajout possibilité de mettre chiffre en entré

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -20,8 +21,14 @@ func fizzBuzz(number int) string {
 
 // soit on donne un chiffre précis en entrée soit on fait sur une fourchette de 1 à 100
 func main() {
-	// sur une fourchette de 1 à 100
-	for i := 1; i <= 100; i++ {
-		fmt.Println(fizzBuzz(i))
+
+	num, _ := strconv.Atoi(os.Args[1])
+	if num != 0 {
+		fmt.Println(fizzBuzz(num))
+	} else {
+		// sur une fourchette de 1 à 100
+		for i := 1; i <= 100; i++ {
+			fmt.Println(fizzBuzz(i))
+		}
 	}
 }
